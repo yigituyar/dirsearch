231dc3c6b4fbd0f2d6886a7d5993922ee97c4b77
yiğit uyar <69603552+yigituyar@users.noreply.github.com>
2022-04-26 13:55:30 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace dirsearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btScan = new System.Windows.Forms.Button();
+            this.tbUrl = new System.Windows.Forms.TextBox();
+            this.dgResults = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btScan
+            // 
+            this.btScan.Location = new System.Drawing.Point(71, 190);
+            this.btScan.Name = "btScan";
+            this.btScan.Size = new System.Drawing.Size(159, 44);
+            this.btScan.TabIndex = 1;
+            this.btScan.Text = "Scan";
+            this.btScan.UseVisualStyleBackColor = true;
+            this.btScan.Click += new System.EventHandler(this.btScan_Click);
+            // 
+            // tbUrl
+            // 
+            this.tbUrl.Location = new System.Drawing.Point(38, 154);
+            this.tbUrl.Name = "tbUrl";
+            this.tbUrl.Size = new System.Drawing.Size(219, 20);
+            this.tbUrl.TabIndex = 2;
+            // 
+            // dgResults
+            // 
+            this.dgResults.BackgroundColor = System.Drawing.Color.Teal;
+            this.dgResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgResults.Location = new System.Drawing.Point(447, 170);
+            this.dgResults.Name = "dgResults";
+            this.dgResults.Size = new System.Drawing.Size(258, 163);
+            this.dgResults.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(362, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Results :";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.BlinkRate = 400;
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 33);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Select Wordlist File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(296, 124);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(130, 186);
+            this.listBox1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(481, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "lines.Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(481, 66);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "label2";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(538, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(481, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(538, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(481, 110);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 13);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "label2";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(538, 110);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(35, 13);
+            this.label10.TabIndex = 7;
+            this.label10.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(205)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(687, 431);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgResults);
+            this.Controls.Add(this.tbUrl);
+            this.Controls.Add(this.btScan);
+            this.Name = "Form1";
+            this.Text = "Directory Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgResults)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btScan;
+        private System.Windows.Forms.TextBox tbUrl;
+        private System.Windows.Forms.DataGridView dgResults;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+using System.Net;
+
+namespace dirsearch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ErrorProvider provider = new ErrorProvider();  
+
+        bool resized = false;
+        void resize(int height)
+        {
+            for (int i = 300; i < height; i++)
+            {
+                this.Size = new Size(i, 300);
+
+            }
+
+        }
+
+
+        
+        string read(string filename,int indexnumber)
+        { // To read a text file line by line
+
+            File.Exists(filename);
+            // Store each line in array of strings
+            string[] lines = File.ReadAllLines(filepath);
+            //length = lines.Length;
+
+            return lines[indexnumber];
+            
+        }
+        
+        HttpStatusCode statusCode = new HttpStatusCode();
+        
+        string url;
+        string get(string site ,string dizinadi )
+        {
+            string directoryName = dizinadi;
+            url = site + dizinadi + "/";
+            try
+            {
+                HttpWebRequest myHttpWebRequest = (HttpWebRequest)WebRequest.Create("https://google.com/");
+                HttpWebResponse myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+                //myHttpWebResponse = (HttpWebResponse)myHttpWebRequest.GetResponse();
+                //statusCode = myHttpWebResponse.StatusCode;
+
+
+            }
+            catch (Exception we)
+            {
+                MessageBox.Show(we.ToString());
+
+            }
+            return statusCode.ToString();
+        }
+
+
+
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.jet.oledb.4.0; Data Source=ogrenciler.mdb");
+        string filepath;
+        void connect()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+        void disconnect()
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+        }
+        //void add()
+        //{
+        //    string sorgu = "insert into ogrtablo(ogr_ad,ogr_no,ogr_tc,ogr_ort) values(@siteName,@dirName,@statusCode,@length)";
+        //    OleDbCommand komut = new OleDbCommand(sorgu, connection);
+        //    komut.Parameters.AddWithValue("@siteName", domain );
+        //    komut.Parameters.AddWithValue("@dirName", ln );
+        //    komut.Parameters.AddWithValue("@statusCode", );
+        //    komut.Parameters.AddWithValue("@length", );
+            
+
+        //    OleDbDataAdapter adp = new OleDbDataAdapter(sorgu, connection);
+
+        //    DataTable tablo = new DataTable();
+        //    adp.Fill(tablo);
+        //    list();
+        //}
+        void list()
+        {
+            string query = "select * from "+ domain;
+            OleDbDataAdapter adp = new OleDbDataAdapter(query, connection);
+            DataTable tablo = new DataTable();
+            adp.Fill(tablo);
+            dgResults.DataSource = tablo;
+        }
+        void create()
+        {
+            string sorgu = "CREATE TABLE "+domain+" AS SELECT * FROM mainTable;";
+            OleDbCommand komut = new OleDbCommand(sorgu, connection);
+            
+
+            OleDbDataAdapter adp = new OleDbDataAdapter(sorgu, connection);
+
+            DataTable tablo = new DataTable();
+            adp.Fill(tablo);
+            list();
+        }
+        string domain;
+        private void btScan_Click(object sender, EventArgs e)
+        {
+            
+            int charPos = tbUrl.Text.LastIndexOf("\\");
+
+            string asd = tbUrl.Text.Substring(charPos + 1);
+            domain = asd;
+
+            if (tbUrl.Text.StartsWith("http"))
+            {
+                if (resized == false )
+                {
+                    resize(750);
+                    resized = true;
+                    
+
+                }
+                //else
+                //    //scan();
+            }
+            else
+            {
+
+                provider.SetError(tbUrl, "url formatı 'http://****.***' veya 'https://****.***' şeklinde olmalıdır");
+            }
+
+           
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Size = new Size(300, 300);
+            get("https://stackoverflow.com/", "");
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {
+                InitialDirectory = @"D:\",
+                Title = "Browse Text Files",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "txt",
+                Filter = "txt files (*.txt)|*.txt",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                 filepath = openFileDialog1.FileName;
+                //int charPos = filepath.LastIndexOf("\\");
+               
+                //file = filepath.Substring(charPos+1);
+                //button1.Text = file;
+            }  
+           
+        }
+        //void scan()
+        ////{
+        ////    string bisey ;
+        ////    for (int i = 0; i < 100; i++)
+        ////    {
+        ////        bisey = read(file,i);
+        ////        listBox1.Items.Add( get(tbUrl.Text,bisey));
+                
+
+ 
+        ////    }
+        //    //MessageBox.Show();
+            
+        //}
+    }
+}
